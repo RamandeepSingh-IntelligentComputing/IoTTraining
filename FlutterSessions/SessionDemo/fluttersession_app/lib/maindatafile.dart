37fe07ce94990bd258ad4da7e0c53bf314c11a50
ramandeep.singh <ramandeep.singh@grplabs.app>
2023-03-03 18:29:25 +0530

    BasicWidgets Covered

---
+++ FlutterSessions/SessionDemo/fluttersession_app/lib/maindatafile.dart
@@ -0,0 +1,226 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttersession_app/DisplayWidget.dart';
+import 'package:fluttersession_app/FutureDemo.dart';
+import 'package:fluttersession_app/IDWidget.dart';
+import 'package:fluttersession_app/StatefulDemoWidget.dart';
+import 'package:fluttersession_app/screen1.dart';
+import 'package:fluttersession_app/screen2.dart';
+
+import 'ColorWidget.dart';
+import 'CounterWidget.dart';
+import 'FBuilder.dart';
+import 'bledemo.dart';
+import 'globalDef.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+List<student> st = List.generate(5, (index) => student('Student $index','Msc'));
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return(
+        MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              appBar: AppBar(title: Center(child:Text("Login UI"),)),
+              drawer: Drawer(child: drawerColumn(context),),
+              body: bleDemo(),
+            )
+        ));
+  }
+}
+
+class Holder extends  StatelessWidget{
+  //CounterWidget cw = CounterWidget();
+  DisplayWidget cw = DisplayWidget();
+  @override
+  Widget build(BuildContext context) {
+    return(
+        Container(child:Column(
+          children: [
+            Expanded(child: cw),
+            //IDWidget(incr:cw.cws.incr,decr:cw.cws.decr),
+            IDWidget(display:cw.cws.display,),
+          ],
+        ))
+    );
+  }
+
+}
+
+
+class dynamicStudentList extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+
+    return(Container(
+        child:Column(children: [
+          Expanded(child: ListView.builder(itemCount:st.length,
+              itemBuilder: (BuildContext context,int index){
+                return studentRow(st[index]);
+              })),
+          ElevatedButton(onPressed: (){
+            st.add(student('New Student 5','MCA'));
+            print('Data Added');
+          }, child: Text("Add")),
+        ],)
+    ));
+  }
+}
+
+Widget studentRow(student s){
+  return(
+      ListTile(leading: Icon(Icons.account_circle,size: 30,),title: Text(" ${s.name} "),
+          subtitle: Text("${s.collage}"),trailing: ElevatedButton(child: Text("..."),onPressed: (){},))
+  );
+}
+
+Widget drawerColumn(BuildContext context){
+  return Container(
+      child:Column(mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.agriculture_sharp,size: 150,color: Colors.black,),
+          Icon(Icons.anchor_rounded,size: 150,color: Colors.black,),
+          Icon(Icons.android_outlined,size: 150,color: Colors.black,),
+        ],
+      ));
+}
+
+/*
+class studentList extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return(
+        ListView(
+          children: [
+            ListTile(leading: Icon(Icons.account_circle,size: 30,),title: Text("Student 1"),
+              subtitle: Text("B.Tech"),trailing: ElevatedButton(child: Text("..."),onPressed: (){},),),
+            ListTile(leading: Icon(Icons.account_circle,size: 30,),title: Text("Student 2"),
+              subtitle: Text("B.Tech"),trailing: ElevatedButton(child: Text("..."),onPressed: (){},),),
+            ListTile(leading: Icon(Icons.account_circle,size: 30,),title: Text("Student 3"),
+              subtitle: Text("B.Tech"),trailing: ElevatedButton(child: Text("..."),onPressed: (){},),),
+            ListTile(leading: Icon(Icons.account_circle,size: 30,),title: Text("Student 4"),
+              subtitle: Text("B.Tech"),trailing: ElevatedButton(child: Text("..."),onPressed: (){},),)
+          ],
+        )
+    );
+  }
+}
+
+class loginUi extends StatelessWidget{
+
+  TextEditingController lc = TextEditingController();
+  TextEditingController pc = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return(
+        Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(2.0),color:Colors.blueGrey),
+            child:Stack(children:[
+              mainContainer(context,lc,pc),
+              Align(child:buttonRow(context,lc,pc),alignment: Alignment.bottomCenter),
+            ])
+        )
+    );
+  }
+}
+
+Widget mainContainer(BuildContext context,TextEditingController lc, TextEditingController pc)
+{
+  return(
+   Column(mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+      Padding(child:loginRow(context,lc),padding: EdgeInsets.all(10.0),),
+        Padding(child:passRow(context,pc),padding: EdgeInsets.all(10.0),),
+
+    ],
+  )
+  );
+}
+
+Widget loginRow(BuildContext context,TextEditingController lc)
+{
+  return(
+      Row(mainAxisAlignment: MainAxisAlignment.center,
+      children:  [
+        const Text("Login ",style: TextStyle(decoration: TextDecoration.none,
+            fontSize: 18,color:Colors.black ),),
+        Expanded(child:Material(child:TextField(controller: lc,)))
+      ],
+  )
+  );
+}
+
+Widget passRow(BuildContext context,TextEditingController pc)
+{
+  return(
+  Row(
+        children: [
+          const Text("Password ",style: TextStyle(decoration: TextDecoration.none,
+              fontSize: 18,color:Colors.black ),),
+          Expanded(child: Material(child:TextField(controller: pc,obscureText: true,)))
+        ],
+      )
+  );
+}
+
+Widget buttonRow(BuildContext context,TextEditingController lc,TextEditingController pc)
+{
+  return(
+  Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: [
+      ElevatedButton(onPressed: (){lc.text="";}, child: Text("Sign Up")),
+      ElevatedButton(onPressed: (){print("Login Pressed - ${lc.text} - ${pc.text}");}, child: Text("Login"))
+    ],
+  )
+  );
+}
+
+Widget ComponentRow(BuildContext context){
+  return(
+  Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: [
+      DisplayComopnent(context, "1"),
+      DisplayComopnent(context, "2"),
+      DisplayComopnent(context, "3"),
+      DisplayComopnent(context, "4"),
+    ],
+  )
+  );
+}
+
+Widget ComponentList(BuildContext context){
+  return(
+  Column(mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.center,
+
+    children: [
+      DisplayComopnent(context,"Display Component 1"),
+      DisplayComopnent(context,"Display Component 2"),
+      DisplayComopnent(context,"Display Component 3"),
+      DisplayComopnent(context,"Display Component 4"),
+    ],
+  )
+  );
+}
+
+Widget DisplayComopnent(BuildContext contex,String msg)
+{
+  return(
+  Padding( padding: EdgeInsets.all(5.0),
+        child:Text("Hello $msg",style: TextStyle(fontSize: 20),)
+  ));
+}
+*/
